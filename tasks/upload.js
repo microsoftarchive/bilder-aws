daa44fa54f4e28a6ad032804a40252442eb235f8
Aditya <aditya@netroy.in>
2014-04-15 14:53:46 +0200

    split the code for testing

--- tasks/upload.js
+++ tasks/upload.js
@@ -4,94 +4,15 @@ module.exports = function (grunt) {
 
   var fs = require('fs');
   var path = require('path');
-  var crypto = require('crypto');
-  var zlib = require('zlib');
 
   var async = require('async');
-  var mime = require('mime');
-  var knox = require('knox');
-  var debounce = require('debounce');
 
-  var tmp = require('tmp');
-  tmp.setGracefulCleanup();
-
-  var cache;
-  var cacheFile = path.resolve(process.env.HOME, '.s3.cache.json');
-  try {
-    cache = require(cacheFile);
-  } catch (e) {
-    cache = {};
-  }
-
-  var defaults = {
-    'throttle': 10,
-    'hash': true,
-    'gzip': false,
-    'baseDir': 'public',
-    'buildDir': 'build',
-    'prefix': ''
-  };
+  var defaults = require('../lib/defaults');
+  var hashFile = require('../lib/hashFile');
+  var gzipFile = require('../lib/gzipFile');
+  var upload = require('../lib/uploadFile');
 
   var encodingOptions = { 'encoding': 'utf8' };
-  var updateCache = debounce(function (key) {
-    if (key) {
-      cache[key] = true;
-    }
-    var data = JSON.stringify(cache, null, 2);
-    fs.writeFileSync(cacheFile, data, encodingOptions);
-  }, 50, true);
-
-  function hashFile (local, callback) {
-    var shasum = crypto.createHash('sha1');
-    var stream = fs.ReadStream(local);
-    stream.on('data', shasum.update.bind(shasum));
-    stream.on('end', function() {
-      callback(null, shasum.digest('hex'));
-    });
-  }
-
-  function gzipFile (local, callback) {
-    tmp.file(function (err, gzipped) {
-      var gzip = zlib.createGzip();
-      var inStream = fs.ReadStream(local);
-      var outStream = fs.WriteStream(gzipped);
-      inStream.pipe(gzip).pipe(outStream);
-      inStream.on('end', function() {
-        setTimeout(function () {
-          callback(null, gzipped);
-        }, 100);
-      });
-    });
-  }
-
-  var uploader;
-  function upload (name, local, remote, isGzip, callback) {
-
-    var headers = {
-      'Content-Type': mime.lookup(name),
-      'Content-Length': fs.statSync(local).size,
-      'x-amz-acl': 'public-read',
-      'Access-Control-Allow-Origin': '*'
-    };
-
-    if (isGzip) {
-      headers['Content-Encoding'] = 'gzip';
-    }
-
-    var stream = fs.ReadStream(local);
-    uploader.putStream(stream, remote, headers, function (err, response) {
-      if (err || response.statusCode !== 200) {
-        // TODO: add re-trial
-        grunt.log.error('\u2717'.red, name);
-      } else {
-        grunt.log.ok('\u2713'.green, name);
-        updateCache(remote);
-        process.nextTick(function () {
-          callback(null, remote);
-        });
-      }
-    });
-  }
 
   function processFile (options, file, callback) {
     // get the absolute path
@@ -106,13 +27,22 @@ module.exports = function (grunt) {
       fileStat = fs.lstatSync(local);
     }
 
+    function done (err, remote) {
+      if (err) {
+        grunt.log.error('\u2717'.red, name);
+      } else {
+        grunt.log.ok('\u2713'.green, name);
+      }
+      callback(null, remote);
+    }
+
     var prefix = options.prefix;
     var args = [
       name,
       local,
       null,
       !!options.gzip,
-      callback
+      done
     ];
 
     hashFile(local, function (err, shasum) {
@@ -122,7 +52,7 @@ module.exports = function (grunt) {
                :path.join(prefix, original);
 
       // skip upload, if already uploaded
-      if (options.hash && cache[remote]) {
+      if (options.hash && upload.cache[remote]) {
         grunt.log.debug('\u25C8'.yellow, name);
         return callback(null, remote);
       }
@@ -152,27 +82,32 @@ module.exports = function (grunt) {
       'cwd': options.baseDir
     }, that.data.src);
 
-    var fn = processFile.bind(null, options);
-
     // init the uploader
-    uploader = knox.createClient({
+    upload.init({
       'key': options.key,
       'secret': options.secret,
       'bucket': options.bucket
     });
 
     var max = options.throttle;
+    var fn = processFile.bind(null, options);
     async.mapLimit(files, max, fn, function (err, remotes) {
-      var map = {};
-      remotes.forEach(function (remote, index) {
-        map[files[index]] = remote;
-      });
-      var mapFile = path.resolve(options.buildDir, that.target + '.json');
-      fs.writeFileSync(mapFile, JSON.stringify(map, null, 2), encodingOptions);
+
+      // save the hashes
+      if (options.hash) {
+        var map = {};
+        remotes.forEach(function (remote, index) {
+          map[files[index]] = remote;
+        });
+        var mapFile = path.resolve(options.buildDir, that.target + '.json');
+        var data = JSON.stringify(map, null, 2);
+        fs.writeFileSync(mapFile, data, encodingOptions);
+      }
 
       setTimeout(done, 200);
     });
   }
 
-  grunt.registerMultiTask('bilder/upload', 'Upload to a S3 bucket', S3UploadTask);
+  grunt.registerMultiTask('bilder/upload',
+      'Upload to a S3 bucket', S3UploadTask);
 };
