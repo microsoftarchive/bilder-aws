24444140b7c3874b4e037e437186630b976cef3b
Aditya <aditya@netroy.in>
2014-04-15 14:53:57 +0200

    working download task

--- tasks/download.js
+++ tasks/download.js
@@ -1,10 +1,37 @@
-module.exports = function(grunt, options) {
+module.exports = function (grunt) {
 
   'use strict';
 
-  function S3DownloadTask () {
+  var async = require('async');
 
+  var download = require('../lib/downloadFile');
+
+  var defaults = {
+    'destDir': 'build'
+  };
+
+  function S3DownloadTask () {
+    var that = this;
+    var options = that.options(defaults);
+    var done = that.async();
+
+    // init the downloader
+    download.init({
+      'key': options.key,
+      'secret': options.secret,
+      'bucket': options.bucket
+    });
+
+    var files = that.data.src;
+    var fn = function (file, callback) {
+      download(options, file, function () {
+        grunt.log.ok('\u2713'.green, file);
+        process.nextTick(callback);
+      });
+    };
+    async.forEachLimit(files, 4, fn, done);
   }
 
-  grunt.registerTask('bilder/download', 'Get objects from a s3 bucket', S3DownloadTask);
+  grunt.registerMultiTask('bilder/download',
+    'Get objects from a s3 bucket', S3DownloadTask);
 };
